163665b53d4cc4280ecd0f08ff3cb76c086b39a6
Narbonneau Paul <paul.narbonneau-ext@socgen.com>
2018-03-22 17:26:09 +0100

    Chapitre 2 Way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,42 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mon-app',
-  template: `<h1>Hello {{name}}</h1>
-              <fieldset>
-                <img [src]="image"/>
-              </fieldset>
-              <label [style.color] = "color">Couleur préférée : </label>
-              <br/>
-              <button (click)="clicked()">Changer Couleur</button>
-              <!-- Methode possible avec le #selector
-              <select #selector (change)="changeColor(selector.value)" >
-              -->
-              <select (change)="changeColor($event.target.value)" >
-                <option>red</option>
-                <option>purple</option>
-                <option>blue</option>
-                <option>green</option>
-              </select>
-              <br/>
-              <button (click)="adressClick()">Afficher/cacher Adresse</button>
-              <div [hidden]="hideAdress">
-              <fieldset>
-                <label [style.color] = "color">Adresse : {{street}}</label>
-                </fieldset>
-                <fieldset>
-                <label [style.color] = "color">Ville : {{city}}</label>
-                </fieldset>
-              </div>
-              <select (change)="regionChanged($event.target.value)">
-                <option>Est</option>
-                <option>Ouest</option>
-                <option>Nord</option>
-                <option>Sud</option>
-              </select>
-              <p>
-                {{name}} est dans la région {{region}}
-              </p>
-  `,
+  templateUrl: './app.component.html',
 })
 export class AppComponent  { 
   
@@ -54,4 +19,5 @@ export class AppComponent  {
   changeColor(color : string){ this.color = color;}
   adressClick() { this.hideAdress = !this.hideAdress ;}
   regionChanged(region : string) { this.region = region;}
+  nameChanged(name : string) { this.name = name;}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
 // decorator qui sert de metadata pour décrire la classe 
 @NgModule({
-  imports:      [ BrowserModule ], // de quoi j'ai besoin
+  imports:      [ BrowserModule, FormsModule ], // de quoi j'ai besoin
   declarations: [ AppComponent ], // de quoi est composée mon app
   bootstrap:    [ AppComponent ] // ou je demarre
 })
