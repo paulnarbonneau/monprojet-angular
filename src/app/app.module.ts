1dab119faee34cba5b64e1ab1c34ef30729e7bc2
Narbonneau Paul <paul.narbonneau-ext@socgen.com>
2018-03-22 15:07:50 +0100

    Apres Exercice 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+// decorator qui sert de metadata pour décrire la classe 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule ], // de quoi j'ai besoin
+  declarations: [ AppComponent ], // de quoi est composée mon app
+  bootstrap:    [ AppComponent ] // ou je demarre
 })
 export class AppModule { }
