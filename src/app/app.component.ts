825194a1599955832ba511595975774d0cd7b7df
Paul Narbonneau <pnarbonn@gmail.com>
2018-03-30 13:46:50 +0200

    Cours Angular Models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,35 @@
 import { Component } from '@angular/core';
 
+import { Customer } from './model';
+
 @Component({
   selector: 'mon-app',
   templateUrl: './app.component.html',
 })
 export class AppComponent  { 
   
-  name = 'Paul Narbonneau'; 
   image = 'favicon.ico';
   color = 'blue';
-
-  street = 'Ile Marante';
-  city = 'Colombes';
-  region = 'France';
+  
   hideAdress = false;
+  
+  // On peut utiliser une notation java avec new + constructeur 
+  // customer : Customer = new Customer();
+  // On peut utiliser la notation JSON pour construire l'objet
+  customer : Customer = {
+    id : 1,
+    name : 'Paul Narbonneau',
+    adress : { 
+      street : 'Ile Marante',
+      city : 'Colombes',      
+      region : 'Ouest',
+      state : 'France'
+    }
+  };
 
   clicked(){ this.color = this.color === 'red' ? 'blue' :'red' ;}
   changeColor(color : string){ this.color = color;}
   adressClick() { this.hideAdress = !this.hideAdress ;}
-  regionChanged(region : string) { this.region = region;}
-  nameChanged(name : string) { this.name = name;}
+  regionChanged(region : string) { this.customer.adress.region = region;}
+  nameChanged(name : string) { this.customer.name = name;}
 }
---
+++ src/app/model.ts
@@ -0,0 +1,16 @@
+export class Customer
+{
+    id : number;
+    name : string;
+    adress : Adress;
+
+}
+
+export class Adress{
+    
+    city : string;
+    street : string;
+    region : string;
+    state : string;
+
+}
