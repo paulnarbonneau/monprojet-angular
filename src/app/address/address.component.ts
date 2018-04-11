73c15327aa215a8edddcd6a6e87c1a7c56c99935
Paul Narbonneau <pnarbonn@gmail.com>
2018-04-11 11:37:08 +0200

    Commit Multiple Components

--- src/app/address/address.component.ts
+++ src/app/address/address.component.ts
@@ -13,16 +13,10 @@ export class AddressComponent  {
 
 
     @Input() address : Address;
-
-    showAddress = true;
     
     regions = ['West','Noth','East','South','Midwest'];
 
     states = ['California', 'Illinois','Quebec','Jalisco'];
 
-      //clicked(){ this.color = this.color === 'red' ? 'blue' :'red' ;}
-  //changeColor(color : string){ this.color = color;}
-  addressClick() { this.showAddress = !this.showAddress ;}
-  //regionChanged(region : string) { this.customer.address.region = region;}
-  //nameChanged(name : string) { this.customer.name = name;}
+  
 }
